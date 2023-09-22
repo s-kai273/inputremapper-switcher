0aee1c2837e9cceec144f8ecfdeb505211cd9383
s-kai273 <kinoshita116@gmail.com>
2023-09-22 18:59:47 +0900

    change input-remapper-control being executed in background

--- inputremapper_switcher.sh
+++ inputremapper_switcher.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+SLEEP_INTERVAL=1
 BASH_COMMAND=`xdotool getwindowfocus getwindowname`
 PreFocusApp=""
 
@@ -8,18 +9,18 @@ do
   if [ "$FocusApp" = "$PreFocusApp" ] ; then
     echo "## continue"
     PreFocusApp=$FocusApp
-    sleep 1
+    sleep $SLEEP_INTERVAL
     continue
   fi
 
   if [ "$PreFocusApp" = "$BASH_COMMAND" ] && [ "$FocusApp" != "$BASH_COMMAND" ] ; then
     echo "## start"
-    input-remapper-control --command start --device "AT Translated Set 2 keyboard" --preset "gui keybindings"
+    input-remapper-control --command start --device "AT Translated Set 2 keyboard" --preset "gui keybindings" &
   elif [ "$PreFocusApp" != "$BASH_COMMAND" ] && [ "$FocusApp" = "$BASH_COMMAND" ] ; then
     echo "## stop"
-    input-remapper-control --command stop --device "AT Translated Set 2 keyboard"
+    input-remapper-control --command stop --device "AT Translated Set 2 keyboard" &
   fi
 
   PreFocusApp=$FocusApp
-  sleep 1
+  sleep $SLEEP_INTERVAL
 done
